f67771a7d7f49f2cfa136b7e5c8b9f9803fad247
Mavilimaa <00001079312456sp@al.educacao.sp.gov.br>
2024-11-20 14:42:11 -0300

    adicionando arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Escola',
+    'Quem fa parte do clube do livro?',
+    'Bianca Ramos, Caio, Manu, Mavi, Passos e Vitorinha'
+)
+
+criaCartao(
+    'Vida',
+    'Mavi namora?',
+    'Sim, o feio do Kauan'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se pede para ir ao banheiro em inglês?',
+    'Can I go to the bathroom?'
+)
+
+criaCartao(
+    'Aulas',
+    'Qual a melhor aula pra dormir?',
+    'As aulas da Maria Cecília'
+)
